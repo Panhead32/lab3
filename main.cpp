c9bbf947dca0ae10fc185c7baf21b7b9a471908b
Roman <54202786+Panhead32@users.noreply.github.com>
2021-10-19 22:27:09 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+﻿/*
+* Пример программы, работающей со своей командной строкой
+*/
+#include <stdio.h>
+#include <string.h>
+int main(int argc, char **argv) {
+    if(argc <= 1) {
+        printf("syntax : \n\t%s --help or\n\t./p --add [a] [b]\n", argv[0]);
+        return 0;
+    }
+
+    if(!strcmp(argv[1], "--help") || !strcmp(argv[1], "-h")) {
+        printf("You requested help message.\n");
+    }else 
+    if(!strcmp(argv[1], "-l") || !strcmp(argv[1], "--list")) {
+        printf("You requested list message.\n");
+    } else
+    if(!strcmp(argv[1], "-v") || !strcmp(argv[1], "--version")) {
+        printf("0.1.1.\n");
+    } else {
+        printf("Unknown parameter: '%s'. Type %s --help for help.\n", argv[1], argv[0]);
+    }
+    
+    return 0;
+}
